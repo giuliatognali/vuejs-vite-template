0eafccb79172871bd3a0d11b98375a77fc52bd59
giuliatognali <118864811+giuliatognali@users.noreply.github.com>
2023-05-28 00:59:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Vue e Vite Template
+Template di partenza per progetti Vue con option API
+
+Per installare il progetto:
+- npm install
+- npm run dev
---
+++ src/components/HelloApp.vue
@@ -0,0 +1,16 @@
+<script>
+export default{
+  name: 'HelloApp'
+}
+</script>
+
+<template>
+    <h1>Hello!</h1>
+</template>
+
+<style lang="scss" scoped>
+h1{
+    color:blue
+}
+
+</style>
